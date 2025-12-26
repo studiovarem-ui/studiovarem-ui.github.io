aa4e1f408af52ff46b0f31ecfbd5d98cf59a74cc
studiovarem-ui <studio.varem@gmail.com>
2025-12-27 08:24:45 +0900

    001.js 만들기

---
+++ qt/data/001.js
@@ -0,0 +1,20 @@
+ADD([
+  {
+    ref_ko: "시편 23:1-2",
+    ref_en: "Psalm 23:1-2",
+    ko: "여호와는 나의 목자시니 내가 부족함이 없으리로다\n그가 나를 푸른 초장에 누이시며 쉴 만한 물가로 인도하시는도다",
+    en: "The LORD is my shepherd; I shall not want.\nHe maketh me to lie down in green pastures: he leadeth me beside the still waters."
+  },
+  {
+    ref_ko: "잠언 3:5-6",
+    ref_en: "Proverbs 3:5-6",
+    ko: "너는 마음을 다하여 여호와를 의뢰하고 네 명철을 의지하지 말라\n너는 범사에 그를 인정하라 그리하면 네 길을 지도하시리라",
+    en: "Trust in the LORD with all thine heart; and lean not unto thine own understanding.\nIn all thy ways acknowledge him, and he shall direct thy paths."
+  },
+  {
+    ref_ko: "마태복음 6:33",
+    ref_en: "Matthew 6:33",
+    ko: "너희는 먼저 그의 나라와 그의 의를 구하라 그리하면 이 모든 것을 너희에게 더하시리라",
+    en: "But seek ye first the kingdom of God, and his righteousness; and all these things shall be added unto you."
+  }
+]);
