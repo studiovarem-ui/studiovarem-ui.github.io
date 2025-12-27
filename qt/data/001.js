0a5113ba3363731a34d2764004bbaaa3f65a3a7c
studiovarem-ui <studio.varem@gmail.com>
2025-12-27 13:08:44 +0900

    001.js 업데이트

--- qt/data/001.js
+++ qt/data/001.js
@@ -58,5 +58,65 @@ ADD([
     ref_en: "Philippians 4:13",
     ko: "내게 능력 주시는 자 안에서 내가 모든 것을 할 수 있느니라",
     en: "I can do all things through Christ which strengtheneth me."
+  },
+  {
+    ref_ko: "로마서 8:28",
+    ref_en: "Romans 8:28",
+    ko: "우리가 알거니와 하나님을 사랑하는 자 곧 그의 뜻대로 부르심을 입은 자들에게는 모든 것이 합력하여 선을 이루느니라",
+    en: "And we know that all things work together for good to them that love God, to them who are the called according to his purpose."
+  },
+  {
+    ref_ko: "요한복음 14:27",
+    ref_en: "John 14:27",
+    ko: "평안을 너희에게 끼치노니 곧 나의 평안을 너희에게 주노라 내가 너희에게 주는 것은 세상이 주는 것과 같지 아니하니라 너희는 마음에 근심하지도 말고 두려워하지도 말라",
+    en: "Peace I leave with you, my peace I give unto you: not as the world giveth, give I unto you. Let not your heart be troubled, neither let it be afraid."
+  },
+  {
+    ref_ko: "시편 46:1",
+    ref_en: "Psalm 46:1",
+    ko: "하나님은 우리의 피난처시요 힘이시니 환난 중에 만날 큰 도움이시라",
+    en: "God is our refuge and strength, a very present help in trouble."
+  },
+  {
+    ref_ko: "시편 119:105",
+    ref_en: "Psalm 119:105",
+    ko: "주의 말씀은 내 발에 등이요 내 길에 빛이니이다",
+    en: "Thy word is a lamp unto my feet, and a light unto my path."
+  },
+  {
+    ref_ko: "야고보서 1:5",
+    ref_en: "James 1:5",
+    ko: "너희 중에 누구든지 지혜가 부족하거든 모든 사람에게 후히 주시고 꾸짖지 아니하시는 하나님께 구하라 그리하면 주시리라",
+    en: "If any of you lack wisdom, let him ask of God, that giveth to all men liberally, and upbraideth not; and it shall be given him."
+  },
+  {
+    ref_ko: "잠언 1:7",
+    ref_en: "Proverbs 1:7",
+    ko: "여호와를 경외하는 것이 지식의 근본이거늘 미련한 자는 지혜와 훈계를 멸시하느니라",
+    en: "The fear of the LORD is the beginning of knowledge: but fools despise wisdom and instruction."
+  },
+  {
+    ref_ko: "미가 6:8",
+    ref_en: "Micah 6:8",
+    ko: "사람아 주께서 선한 것이 무엇임을 네게 보이셨나니 여호와께서 네게 구하시는 것은 오직 정의를 행하며 인자를 사랑하며 겸손하게 네 하나님과 함께 행하는 것이 아니냐",
+    en: "He hath shewed thee, O man, what is good; and what doth the LORD require of thee, but to do justly, and to love mercy, and to walk humbly with thy God?"
+  },
+  {
+    ref_ko: "시편 121:1-2",
+    ref_en: "Psalm 121:1-2",
+    ko: "내가 산을 향하여 눈을 들리라 나의 도움이 어디서 올까\n나의 도움은 천지를 지으신 여호와에게서로다",
+    en: "I will lift up mine eyes unto the hills, from whence cometh my help.\nMy help cometh from the LORD, which made heaven and earth."
+  },
+  {
+    ref_ko: "이사야 26:3",
+    ref_en: "Isaiah 26:3",
+    ko: "주께서 심지가 견고한 자를 평강하게 하시리니 이는 그가 주를 신뢰함이니이다",
+    en: "Thou wilt keep him in perfect peace, whose mind is stayed on thee: because he trusteth in thee."
+  },
+  {
+    ref_ko: "마태복음 11:28",
+    ref_en: "Matthew 11:28",
+    ko: "수고하고 무거운 짐 진 자들아 다 내게로 오라 내가 너희를 쉬게 하리라",
+    en: "Come unto me, all ye that labour and are heavy laden, and I will give you rest."
   }
 ]);
