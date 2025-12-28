c67c0f9c9414a7af23ede7e4c1fc535f97ff1a76
studiovarem-ui <studio.varem@gmail.com>
2025-12-29 00:18:44 +0900

    001.js 만들기

---
+++ english-m3/data/001.js
@@ -0,0 +1,37 @@
+add([
+  {day:1,  w:"achieve",   p:"v",   m_en:"to succeed in doing something", m_ko:"달성하다", e_en:"He worked hard to achieve his goal.", e_ko:"그는 목표를 달성하기 위해 열심히 노력했다."},
+  {day:2,  w:"admire",    p:"v",   m_en:"to respect and like someone", m_ko:"존경하다", e_en:"I admire my teacher.", e_ko:"나는 선생님을 존경한다."},
+  {day:3,  w:"afford",    p:"v",   m_en:"to have enough money to buy", m_ko:"살 여유가 있다", e_en:"She can't afford a new phone.", e_ko:"그녀는 새 휴대폰을 살 여유가 없다."},
+  {day:4,  w:"allow",     p:"v",   m_en:"to let someone do something", m_ko:"허락하다", e_en:"My parents allow me to go out.", e_ko:"부모님은 내가 나가는 것을 허락하신다."},
+  {day:5,  w:"announce",  p:"v",   m_en:"to make something known publicly", m_ko:"발표하다", e_en:"The school announced the results.", e_ko:"학교는 결과를 발표했다."},
+
+  {day:6,  w:"appear",    p:"v",   m_en:"to become visible or present", m_ko:"나타나다", e_en:"A rainbow appeared after the rain.", e_ko:"비가 온 뒤 무지개가 나타났다."},
+  {day:7,  w:"arrange",   p:"v",   m_en:"to plan or organize", m_ko:"정리하다; 준비하다", e_en:"I arranged a meeting with him.", e_ko:"나는 그와의 만남을 잡았다."},
+  {day:8,  w:"attend",    p:"v",   m_en:"to go to an event or place", m_ko:"참석하다", e_en:"She attended the class every day.", e_ko:"그녀는 매일 수업에 참석했다."},
+  {day:9,  w:"avoid",     p:"v",   m_en:"to keep away from something", m_ko:"피하다", e_en:"Try to avoid making the same mistake.", e_ko:"같은 실수를 하는 것을 피하려고 해."},
+  {day:10, w:"balance",   p:"n",   m_en:"a state of being steady or equal", m_ko:"균형", e_en:"It is important to keep a balance.", e_ko:"균형을 유지하는 것이 중요하다."},
+
+  {day:11, w:"belong",    p:"v",   m_en:"to be part of a group", m_ko:"~에 속하다", e_en:"This book belongs to me.", e_ko:"이 책은 내 것이다."},
+  {day:12, w:"bother",    p:"v",   m_en:"to make someone annoyed or worried", m_ko:"귀찮게 하다", e_en:"Noise bothers me when I study.", e_ko:"공부할 때 소음이 나를 귀찮게 한다."},
+  {day:13, w:"calm",      p:"adj", m_en:"not excited or worried", m_ko:"침착한", e_en:"Stay calm and breathe slowly.", e_ko:"침착하게 천천히 숨을 쉬어."},
+  {day:14, w:"cancel",    p:"v",   m_en:"to decide that something will not happen", m_ko:"취소하다", e_en:"They canceled the trip because of rain.", e_ko:"비 때문에 여행을 취소했다."},
+  {day:15, w:"celebrate", p:"v",   m_en:"to do something special for a happy event", m_ko:"축하하다", e_en:"We celebrated his birthday.", e_ko:"우리는 그의 생일을 축하했다."},
+
+  {day:16, w:"charge",    p:"v",   m_en:"to ask an amount of money", m_ko:"요금을 청구하다", e_en:"They charge 5,000 won for delivery.", e_ko:"그들은 배달비로 5,000원을 청구한다."},
+  {day:17, w:"choose",    p:"v",   m_en:"to decide what you want", m_ko:"고르다", e_en:"Choose one answer.", e_ko:"정답을 하나 고르세요."},
+  {day:18, w:"collect",   p:"v",   m_en:"to gather things", m_ko:"모으다", e_en:"He collects stamps.", e_ko:"그는 우표를 모은다."},
+  {day:19, w:"compare",   p:"v",   m_en:"to look at differences and similarities", m_ko:"비교하다", e_en:"Don't compare yourself to others.", e_ko:"너 자신을 다른 사람과 비교하지 마."},
+  {day:20, w:"complain",  p:"v",   m_en:"to say you are unhappy about something", m_ko:"불평하다", e_en:"He complained about the food.", e_ko:"그는 음식에 대해 불평했다."},
+
+  {day:21, w:"complete",  p:"v",   m_en:"to finish something", m_ko:"완성하다", e_en:"I completed my homework.", e_ko:"나는 숙제를 끝냈다."},
+  {day:22, w:"connect",   p:"v",   m_en:"to join or link", m_ko:"연결하다", e_en:"Connect your phone to the Wi-Fi.", e_ko:"휴대폰을 와이파이에 연결해."},
+  {day:23, w:"consider",  p:"v",   m_en:"to think about something carefully", m_ko:"고려하다", e_en:"Please consider my idea.", e_ko:"내 아이디어를 고려해 줘."},
+  {day:24, w:"continue",  p:"v",   m_en:"to keep doing something", m_ko:"계속하다", e_en:"She continued to practice.", e_ko:"그녀는 계속 연습했다."},
+  {day:25, w:"correct",   p:"adj", m_en:"right; not wrong", m_ko:"정확한", e_en:"That answer is correct.", e_ko:"그 답은 정확해."},
+
+  {day:26, w:"create",    p:"v",   m_en:"to make something new", m_ko:"만들다; 창조하다", e_en:"They created a new game.", e_ko:"그들은 새로운 게임을 만들었다."},
+  {day:27, w:"decide",    p:"v",   m_en:"to choose; to make a decision", m_ko:"결정하다", e_en:"She decided to study harder.", e_ko:"그녀는 더 열심히 공부하기로 결정했다."},
+  {day:28, w:"deliver",   p:"v",   m_en:"to bring something to someone", m_ko:"배달하다; 전달하다", e_en:"The package was delivered today.", e_ko:"소포가 오늘 배달되었다."},
+  {day:29, w:"describe",  p:"v",   m_en:"to say what something is like", m_ko:"묘사하다", e_en:"Can you describe the man?", e_ko:"그 남자를 묘사해 줄 수 있니?"},
+  {day:30, w:"develop",   p:"v",   m_en:"to grow or make something better", m_ko:"발전시키다; 개발하다", e_en:"He developed a new habit.", e_ko:"그는 새로운 습관을 만들었다."}
+]);
