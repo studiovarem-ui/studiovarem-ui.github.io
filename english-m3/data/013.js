1bee64412e182933b1ce4f62ec503589ed134962
studiovarem-ui <studio.varem@gmail.com>
2025-12-29 20:49:37 +0900

    013.js 만들기

---
+++ english-m3/data/013.js
@@ -0,0 +1,7 @@
+add([
+  {day:361, w:"erupt",        p:"v",   m_en:"to explode or burst out suddenly", m_ko:"분출하다; 폭발하다", e_en:"The volcano began to erupt.", e_ko:"화산이 분출하기 시작했다."},
+  {day:362, w:"eject",        p:"v",   m_en:"to force something out", m_ko:"내뿜다; 방출하다", e_en:"The machine ejected hot air.", e_ko:"그 기계는 뜨거운 공기를 내뿜었다."},
+  {day:363, w:"purify",       p:"v",   m_en:"to make something clean or pure", m_ko:"정화하다", e_en:"This system purifies water.", e_ko:"이 시스템은 물을 정화한다."},
+  {day:364, w:"surround",     p:"v",   m_en:"to be all around something", m_ko:"둘러싸다", e_en:"Trees surround the house.", e_ko:"나무들이 집을 둘러싸고 있다."},
+  {day:365, w:"discover",     p:"v",   m_en:"to find something for the first time", m_ko:"발견하다", e_en:"She discovered a new talent.", e_ko:"그녀는 새로운 재능을 발견했다."}
+]);
